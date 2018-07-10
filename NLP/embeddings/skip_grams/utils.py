911b4e08f9333cccaf2df2358519b56bf7729736
Peter-Chou <zhou_00009@126.com>
2018-07-10 15:38:12 +0800

    fix bug in DLProgress class

--- NLP/embeddings/skip_grams/utils.py
+++ NLP/embeddings/skip_grams/utils.py
@@ -11,7 +11,7 @@ class DLProgress(tqdm):
 
     def hook(self, block_num=1, block_size=1, total_size=None):
         self.total = total_size
-        self.update((block_size - self.last_block) * block_size)
+        self.update((block_num - self.last_block) * block_size)
         self.last_block = block_num
 
 
